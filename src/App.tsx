9efa736e86071d711d4a689b73ff1b9cab84f308
Subhanjan Baral <88525846+Subhanjan07@users.noreply.github.com>
2025-03-01 14:14:42 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import React from "react";
+import Chatbot from "./Chatbot";
+
 
 function App() {
   const [count, setCount] = useState(0)
   return (
-    <>
-    </>
-  )
+    <div className="App">
+      <Chatbot />
+    </div>
+  );
 }
 
 export default App
