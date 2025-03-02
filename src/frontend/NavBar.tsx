f39351f860fd4b7a82764db5394848c48dc2a808
Rob Basnet <149448555+basnro01@users.noreply.github.com>
2025-03-01 21:16:06 -0500

    particles

---
+++ src/frontend/NavBar.tsx
@@ -0,0 +1,20 @@
+import { Routes, Route } from "react-router-dom";
+import Chatbot from "../Chatbot";
+import LandingPage from "./LandingPage";
+import Settings from "./Settings";
+import EventsPage from "./EventsPage";
+
+const HomeBar: React.FC = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/chatbot" element={<Chatbot />} />
+        <Route path="/timeline" element={<EventsPage />} />
+        <Route path="/settings" element={<Settings />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default HomeBar;
