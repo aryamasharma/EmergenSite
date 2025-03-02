c121a44acab2222e5d2ee8ab096a7e94e5c4d89d
Subhanjan Baral <88525846+Subhanjan07@users.noreply.github.com>
2025-03-02 01:45:34 -0500

    router fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
 import { useState } from "react";
 import "./App.css";
+import React from "react";
+import Chatbot from "./Chatbot";
+import AlertSystem from "./frontend/AlertSystem";
 import LandingPage from "./frontend/LandingPage";
+import Navbar from "./frontend/NavBar";
+import EventsPage from "./frontend/EventsPage";
+import Settings from "./frontend/Settings";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
       <div className="App">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/chatbot" element={<Chatbot />} />
+        <Route path="/events" element={<EventsPage />} />
+        <Route path="/settings" element={<Settings />} />
+      </Routes>
         <div className="flex flex-col items-center">
           <LandingPage />
         </div>
